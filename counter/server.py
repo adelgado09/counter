26d2db8830f1d891cbdcb930668d21f24e27cbc4
Ashton Delgado <56052219+adelgado09@users.noreply.github.com>
2022-11-29 01:03:12 -0800

    Add files via upload

---
+++ counter/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, session, redirect
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' #set a secret key for security purposes
+
+@app.route('/')
+def index():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session['count'] += 1
+    return render_template("index.html")
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
